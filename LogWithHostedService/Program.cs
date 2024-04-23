2b817af40e873751d76533a710107d235d5dafa0
Alex Pérez <alex@original-soft.com>
2024-04-23 12:24:45 +0200

    Agregar archivos de proyecto.

---
+++ LogWithHostedService/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using LogWithHostedService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) => 
+    {
+        services.AddHostedService<SampleMainService>();
+
+    })
+    .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                .WriteTo.Console())
+    .Build()
+    .Run();
---
+++ LogWithHostedService/SampleMainService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogWithHostedService
+{
+    public class SampleMainService(ILogger<SampleMainService> logger) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("test");
+                Console.WriteLine("ping");
+                await Task.Delay(25);
+            }
+        }
+    }
+}
